c714fe7fe0d03109724731399e6cb5868d60d44b
bswathi29 <bswathi29@gmail.com>
2020-04-28 17:21:42 +0530

    2users testa

--- src/testngFiles/Annotation5.java
+++ src/testngFiles/Annotation5.java
@@ -9,7 +9,16 @@ public class Annotation5 extends TestBase{
 	@Test
 	public void abc() throws IOException
 	{
-	
+		System.out.println("hello hai");
+		System.out.println("hai2");
+		System.out.println("hai3");
+	}
+	@Test
+	public void abcd() throws IOException
+	{
+		System.out.println("hello hai1");
+		System.out.println("hai22");
+		System.out.println("hai33");
 	}
 
 }
